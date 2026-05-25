ff0d5cc06604ae39bd97c6869378558dd7d32ae8
ATUL KUMAR JAISWAL <atulblp04@gmail.com>
2026-05-25 08:18:43 +0530

    adding insertionsort.cpp

---
+++ 4_sorting/insertionsort.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, a[500];
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    // Insertion Sort
+    for(int i = 1; i < n; i++) {
+        int key = a[i];
+        int j = i - 1;
+
+        while(j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
