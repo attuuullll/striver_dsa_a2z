a12b36e62d59d1157498e7b28aa38866a1959614
ATUL KUMAR JAISWAL <atulblp04@gmail.com>
2026-06-09 08:55:09 +0530

    adding BS_20_minimise_maxm_dist_to_gas_station.cpp

---
+++ 7_binary search/BS_20_minimise_maxm_dist_to_gas_station.cpp
@@ -0,0 +1,30 @@
+
+
+
+
+
+// soory im irritated main nhi krpaya ye
+
+
+
+//
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    int k;
+    cin>>k;
+    for(int gas = 1; gas < k; gas++){
+        int min = -1, max = -1;
+        for(int i = 0; i < n; i++){
+            int diff = a[i+1]-a[i];
+            int sectionLength = diff/ 
+        }
+    
+    return 0;
+}
